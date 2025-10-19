bba613658ffe3f47659ceb82ee863537c5320052
inisoliman <one4vc@gmail.com>
2025-10-19 17:14:11 +0300

    Update Deactivator.php

--- includes/Core/Deactivator.php
+++ includes/Core/Deactivator.php
@@ -1,8 +1,25 @@
 <?php
 namespace OrsozoxDivineSEO\Core;
 
+/**
+ * Fired during plugin deactivation
+ */
 class Deactivator {
+    
+    /**
+     * Deactivation tasks
+     */
     public static function deactivate() {
+        // Clear scheduled cron jobs
+        wp_clear_scheduled_hook('odse_daily_analysis');
+        wp_clear_scheduled_hook('odse_weekly_cannibalization_check');
+        
+        // Clear transients
+        global $wpdb;
+        $wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE '_transient_odse_%'");
+        $wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE '_transient_timeout_odse_%'");
+        
+        // Flush rewrite rules
         flush_rewrite_rules();
     }
 }
